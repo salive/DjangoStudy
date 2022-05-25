7c6d622f93cfac3a37dbe5b084e6e678fd5a35e7
Yan Chuprakov <yanchuprakov@yandex.ru>
2022-05-25 21:54:42 +0500

    some refactoring

--- study/imdb/utils/post_utils.py
+++ study/imdb/utils/post_utils.py
@@ -14,7 +14,7 @@ def add_show_to_user_profile(post_request):
     show_title = post_request['title']
     show_poster = post_request['poster']
     show_desription = post_request['desc']
-    show_year = post_request['year']
+    show_year = int(post_request['year'])
     show_rating = float(post_request['rating'])
 
     img_data = requests.get(show_poster).content
@@ -24,9 +24,9 @@ def add_show_to_user_profile(post_request):
     try:
         new_show = Show(id=show_id,
                         title=show_title,
-                        year=int(show_year),
+                        year=show_year,
                         poster=f'imdb\\static\\imdb\\images\\{show_id}.jpg',
-                        rating=float(show_rating),
+                        rating=show_rating,
                         description=show_desription)
         profile = Profile.objects.get(user__username=post_request['user'])
 
